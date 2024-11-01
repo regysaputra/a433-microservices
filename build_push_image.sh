33cad70d35ab81f6535e5aeb33e0327396848ae8
Regy Saputra <regysaputra27@gmail.com>
2024-11-01 19:31:20 +0700

    first submission

--- build_push_image.sh
+++ build_push_image.sh
@@ -2,5 +2,5 @@
 docker build -t item-app:v1 .
 docker images
 docker tag item-app:v1 ghcr.io/regysaputra/item-app:v1
-echo "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" | docker login ghcr.io -u regysaputra --password-stdin
+echo $PASSWORD_GITHUB_PACKAGES | docker login ghcr.io -u regysaputra --password-stdin
 docker push ghcr.io/regysaputra/item-app:v1
